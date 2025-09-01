3987cd9229e138f4020c73a2ac9586654fc05610
Rob Hoelz <rob@hoelz.ro>
2025-09-01 07:20:24 -0500

    Start using local storage annotation store

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-import { annotationStore } from './dummy-store.js';
+import { annotationStore } from './local-storage-store.js';
 import { prompt } from './prompt.js';
 
 const FOLLOW_UP_ID = 'follow-up';
